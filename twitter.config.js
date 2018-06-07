92643e1beb74fac2544f0bd2223746b1b89303f0
kaminikamaliem@gmail.com <kaminikamaliem@gmail.com>
2018-06-07 10:16:57 +0530

    secret key added

--- twitter.config.js
+++ twitter.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  consumerKey: 'KEY',
-  consumerSecret: 'SECRET'
+  consumerKey: 'vc6p6XxqBNdSykuykU819Fua3',
+  consumerSecret: '	7sEX9syWsaGuRmXb63Z1k1R4aLqFgt6cRSn58iIRP9mvTGEYHK'
 };
